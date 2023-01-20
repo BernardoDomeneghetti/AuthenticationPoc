79813da564dc7f9e0d0b0fa11b567884fcff7aad
Bernardo.Oliveira <bernardo.doliveira@linx.com.br>
2023-01-20 14:46:16 -0300

    Structure and model creation

---
+++ AuthenticationPoc/AuthenticationPoc/Controllers/AuthenticationController.cs
@@ -0,0 +1,19 @@
+﻿using AuthenticationPoc.DataTransferObjects;
+using AuthenticationPoc.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationPoc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register (UserDto userDto)
+        {
+
+        }
+    }
+}
