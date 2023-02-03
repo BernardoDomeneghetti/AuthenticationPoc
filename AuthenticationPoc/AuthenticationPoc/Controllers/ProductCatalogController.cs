3a4b0163b7745133b7be0455cf634289df0a409b
Bernardo.Oliveira <bernardo.doliveira@linx.com.br>
2023-02-03 11:49:58 -0300

    Criado HealthCheck e controller para testar a autorização

---
+++ AuthenticationPoc/AuthenticationPoc/Controllers/ProductCatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationPoc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductCatalogController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> ListProducts()
+        {
+            return Ok(new List<string>()
+            {
+                "Produto 1",
+                "Produto 2"
+            });
+        }
+    }
+}
--- AuthenticationPoc/AuthenticationPoc/Program.cs
+++ AuthenticationPoc/AuthenticationPoc/Program.cs
@@ -22,6 +22,9 @@ builder.Services.AddSingleton<IAuthenticationWorker, AuthenticationWorker>();
 
 //RepositoryServices:
 
+//ForeignerServices
+builder.Services.AddSingleton<JwtBearerDefaults.AuthenticationScheme>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
